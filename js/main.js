bcca9b5b46c207be828f1c6b6f1b32a3ff6d29ea
Tonyscode <korobok.avp@gmail.com>
2021-03-15 23:45:21 +0200

    added scroll ended adaptive

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@
  }
 }
 )();
+
+//Burger handler
+(function(){
+   const burgerItem = document.querySelector('.burger');
+   const menu =  document.querySelector('.header__nav');
+   const menuCloseItem = document.querySelector('.header__nav-close');
+   burgerItem.addEventListener('click', ()=> {
+      
+      menu.classList.add('header__nav_active');
+   });
+   menuCloseItem.addEventListener('click', () => {
+      menu.classList.remove('header__nav_active');
+      console.log(menuCloseItem);
+   });
+})();
+
+//Плавный скролл
+$("a.scroll-to").on("click", function(e){
+   e.preventDefault();
+    let anchor = $(this).attr('href');
+   $('html, body').stop().animate({
+       scrollTop: $(anchor).offset().top - 107
+   }, 1000);
+});
